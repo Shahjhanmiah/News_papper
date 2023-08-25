ab85b6a254386686782a20b1844562fe0d91e8e0
masum362 <masumahmed64077@gmail.com>
2023-08-25 23:53:41 +0600

    added bigImage

--- src/components/hero_section/Hero.jsx
+++ src/components/hero_section/Hero.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { news } from './news.js'
 
 
 const Hero = () => {
-    console.log('this is from hero')
+
+
+
+    const BigImage = news[0]
+    console.log(BigImage.Img)
+
     return (
         <div className='relative top-[60px] mt-4 flex justify-between items-center mx-24' >
 
             <div className="big-side">
-                this is big side
+                <img src={BigImage.Img} alt='' />
             </div>
             <div className="list-side">
                 this is list side
