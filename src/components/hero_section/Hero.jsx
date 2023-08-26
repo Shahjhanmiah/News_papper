009ce4c34097525553e1f0924fc737118e6f5ff5
masum362 <masumahmed64077@gmail.com>
2023-08-26 21:23:49 +0600

    masum: completed all news

--- src/components/hero_section/Hero.jsx
+++ src/components/hero_section/Hero.jsx
@@ -11,12 +11,12 @@ const Hero = () => {
     return (
         <div className='mt-[100px] z-[-2] flex justify-between items-center flex-col lg:flex-row' >
 
-            <div className="big-side w-full lg:w-1/2 overflow-hidden ">
+            <div className="big-side w-full lg:w-1/2 h-auto ">
                 <div className='relative top-10 px-2 '>
                     <span className='px-2 bg-black text-white'>জাতীয়</span>
                 </div>
 
-                <img src={BigImage.Img} className='object-cover w-full ' alt='' />
+                <img src={BigImage.Img} className='object-cover w-full h-full' alt='' />
 
                 <div className='bg-black w-[100%]  h-30 px-2 text-white  relative bottom-28  flex flex-col items-center justify-center group-hover:opacity-100 transition-opacity ease-in-out duration-300  bg-opacity-50 '>
 
@@ -31,7 +31,7 @@ const Hero = () => {
             <div className="list-side w-full lg:w-1/2 ml-4 mt-0">
                 {
                     news.slice(1,6).map(item => {
-                        return <div className='flex items-center justify-between pb-4 border-b'>
+                        return <div key={item.id} className='flex items-center justify-between pb-4 border-b'>
                             <h1 className='hover:text-[#1f67ad] cursor-pointer'>{item.title}</h1>
                             <img className='w-12 h-12' src={item.Img} alt="" />
 
