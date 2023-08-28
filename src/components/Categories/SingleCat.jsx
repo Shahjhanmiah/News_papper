daa7ae2b0eadfa453dab706ff75f0e010cc47b36
mdshahjhan <mdshahjha478877@gmail.com>
2023-08-28 22:28:35 +0600

    saju

--- src/components/Categories/SingleCat.jsx
+++ src/components/Categories/SingleCat.jsx
@@ -7,7 +7,7 @@ const SingleCat = ({ BigImage, subtitle, allnews }) => {
         <div className='flex flex-col mx-2 p-2' >
             <div className="heading flex justify-between items-center font-bold text-xl">
                 <h1 className='hover:text-[#1f67ad] cursor-pointer'>{BigImage?.category}</h1>
-                <h1 className='hover:text-[#1f67ad] cursor-pointer'>আরও...>> </h1>
+                <h1 className='hover:text-[#1f67ad] cursor-pointer'>আরও... </h1>
             </div>
             <div className="content ">
                 <img src={BigImage?.Img} alt="" className='cursor-pointer ' />
@@ -17,7 +17,7 @@ const SingleCat = ({ BigImage, subtitle, allnews }) => {
             <div className='related-news'>
                 {allnews?.slice(1, 6).map(item => {
                     return <div>
-                        <p className='py-2 border-b my-2'>><span className='hover:text-[#1f67ad] cursor-pointer text-lg'>{item?.title}</span></p>
+                        <p className='py-2 border-b my-2'><span className='hover:text-[#1f67ad] cursor-pointer text-lg'>{item?.title}</span></p>
                     </div>
                 })}
             </div>
