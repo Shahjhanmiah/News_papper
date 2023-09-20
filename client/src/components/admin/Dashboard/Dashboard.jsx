9688276c447cd76d2e11711c9825865c19ad9beb
Mehedi-Hasan <mdhasanmehedi924@gmail.com>
2023-09-19 23:03:00 -0700

    hasan

--- client/src/components/admin/Dashboard/Dashboard.jsx
+++ client/src/components/admin/Dashboard/Dashboard.jsx
@@ -1,15 +1,58 @@
 import React from 'react'
+import './Dashboard.css'
+import Chart from './../Dashboard/Chart'
 import Sidebar from '../sidebar/Sidebar'
+import Social_visitors from './Social_visitors'
+import adminImg from './../../../../public/hasan.jpg'
+import { FaThLarge, FaUserAlt,FaPuzzlePiece,FaSign} from "react-icons/fa";
+
 
 const Dashboard = () => {
   return (
-    <div className='mt-[100px] grid lg:grid-cols-5 border-2 gap-3'>
-    <div className='cols-start-1 cols-end-2 border-2 border-red-600'>
-    <Sidebar />
-    </div>
-      <div className='col-start-2 col-end-6 border-2 border-green-600'>
-dashboard
-      </div>
+    <div className='mt-[100px] grid sm:grid-cols-1  md:grid-cols-3 lg:grid-cols-5  hy-5 gap-4 '>
+        <div className='cols-start-1 cols-end-2  p-2 bg-slate-700'>
+          <Sidebar />
+        </div>  
+
+        <div className=' Right-site col-start-2 col-end-6   bg-slate-200'> 
+            <div className=' heading-div h-[80px] w-[100%] text-white bg-slate-700 pr-5  lg:pl-[80px] '>
+                <h1 className=' sm:text-1xl md:text-2xl lg:text-5xl pl-3 '> Admin Dashboard </h1>
+                <div className='admin-div'>
+                    <h4> Admin</h4>
+                    <img className=' sm:w-[30px] sm:h-[30px] lg:w-[40px] lg:h-[40px] h-[30px] w-[30px]  rounded-full '  src={adminImg} alt="" />
+                </div>
+            </div>
+             <div className=' grid lg:grid-cols-3 gap-5 px-5 mt-[50px]'>
+                  <div className=' p-5 bg-white'> 
+                               <div className='post-icon'> <FaSign  /> </div>         
+                       <h1 className='text-3xl font-bold '>All Post </h1>
+                       <p className='text-lg font-bold '> Total : 250 +</p>
+                  </div>
+                  <div className=' p-5 bg-white'> 
+                               <div className='post-icon'> <FaUserAlt  /></div>         
+                       <h1 className='text-3xl font-bold '>All Users </h1>
+                       <p className='text-lg font-bold '> Total : 250 +</p>
+                  </div>
+                  <div className=' p-5 bg-white'> 
+                               <div className='post-icon'> <FaPuzzlePiece  /></div>         
+                       <h1 className='text-3xl font-bold '>All Admin </h1>
+                       <p className='text-lg font-bold '> Total : 250 +</p>
+                  </div>                 
+             </div>
+             
+              <div className=' grid lg:grid-cols-3  gap-5  p-5 mt-[20px]'>
+                    <div className=' lg:col-span-2 p-5 bg-white '>
+                        <h1> Chart</h1>
+                           <Chart/>
+                      
+                    </div>
+
+                      <div className='bg-white p-5'>
+                        <h1 className='text-3xl font-bold mb-[50px]'> Social Visitors </h1>
+                        <Social_visitors/>
+                      </div>
+              </div>                   
+         </div>
     </div>
   )
 }
--- client/src/components/admin/sidebar/Sidebar.jsx
+++ client/src/components/admin/sidebar/Sidebar.jsx
@@ -1,9 +1,79 @@
 import React from 'react'
+import DashboardLogo from '../../../../public/Logo.png'
+import { FaThLarge, FaUserAlt, FaPuzzlePiece, FaSign, FaCogs } from "react-icons/fa";
+import { AiOutlineCaretDown } from "react-icons/ai";
+import '../Dashboard/Dashboard.css'
+
 
 const Sidebar = () => {
   return (
-    <div>sidebar</div>
+    <>
+      <div className='Logo'>
+          <img style={{width:"100%" , height:'70px', borderRadius:'0px 10px 0px 10px'}} src={DashboardLogo} alt="" />  
+      </div>
+
+      <div className='menu mt-3 text-white' >
+         <h3 className='text-lg'>Menu</h3>
+         <div className='mt-5 '>
+            {/* <ul >
+              <li className=' font-medium bg-white  text-lg text-black mb-2 br-5 rounded-md flex-auto' ><i >  <FaThLarge /> Dashboard </i> </li>
+                
+              <li className='font-medium bg-white  text-lg text-black mb-2 br-5 rounded-md flex-auto' ><i >  <FaThLarge /> Dashboard </i> </li>
+              <li className='font-medium bg-white  text-lg text-black mb-2 br-5 rounded-md flex-auto' ><i >  <FaSign  /> All Post</i> </li>
+              <li className='font-medium bg-white  text-lg text-black mb-2 br-5 rounded-md flex-auto' ><i >  <FaUserAlt /> All Users</i> </li>
+              <li className='font-medium bg-white  text-lg text-black mb-2 br-5 rounded-md flex-auto' ><i >  <FaPuzzlePiece /> All Admin</i> </li>
+              <li className='font-medium bg-white text-lg text-black mb-2 br-5 rounded-md flex-auto' ><i >  <FaCogs /> Edit</i> </li>
+            </ul> */}
+
+            <div className="dropdown w-[100%] sm:w-[100%] md:w-[100%] xl:w-[100%] lg:w-[100%] ">
+             <button className="dropbtn font-medium bg-white p-2 text-lg text-black mb-2 br-5 rounded-md "> <span >  <i className='flex icon-div '> <FaThLarge />  Dashboard </i> </span>  <sapn  > <AiOutlineCaretDown /> </sapn> </button>    
+              <div className="dropdown-content">
+                  <a href="#">Home</a>
+                  <a href="#"> Dashboard</a>            
+              </div>           
+            </div>
+            <div className="dropdown w-[100%] sm:w-[100%] md:w-[100%] xl:w-[100%] lg:w-[100%] ">
+              <button className="dropbtn font-medium bg-white p-2 text-lg text-black mb-2 br-5 rounded-md "> <span >  <i className='flex icon-div '> <FaSign />    Post </i> </span>  <sapn  > <AiOutlineCaretDown /> </sapn> </button>
+              <div className="dropdown-content">
+                <a href="#">All Post</a>
+                <a href="#">Add New Post</a>
+                <a href="#"> Category</a>
+              </div>           
+            </div>
+            <div className="dropdown w-[100%] sm:w-[100%] md:w-[100%] xl:w-[100%] lg:w-[100%] ">
+              <button className="dropbtn font-medium bg-white p-2 text-lg text-black mb-2 br-5 rounded-md "> <span >  <i className='flex icon-div '> <FaUserAlt />  Users </i> </span>  <sapn  > <AiOutlineCaretDown /> </sapn> </button>
+              <div className="dropdown-content">
+                <a href="#">All Users</a>
+                <a href="#">Add New Users</a>
+                <a href="#">Edite</a>
+              </div>           
+          </div>
+          <div className="dropdown w-[100%] sm:w-[100%] md:w-[100%] xl:w-[100%] lg:w-[100%] ">
+              <button className="dropbtn font-medium bg-white p-2 text-lg text-black mb-2 br-5 rounded-md "> <span >  <i className='flex icon-div '> <FaPuzzlePiece/>  Admin </i> </span>  <sapn  > <AiOutlineCaretDown /> </sapn> </button>
+              <div className="dropdown-content">
+                <a href="#">All Admin </a>
+                <a href="#">Add New Admin</a>
+                <a href="#">Tax</a>
+              </div>           
+          </div>
+           <div className="dropdown w-[100%] sm:w-[100%] md:w-[100%] xl:w-[100%] lg:w-[100%] ">
+              <button className="dropbtn font-medium bg-white p-2 text-lg text-black mb-2 br-5 rounded-md "> <span >  <i className='flex icon-div '> <FaCogs /> Edit </i> </span>  <sapn  > <AiOutlineCaretDown /> </sapn> </button>
+              <div className="dropdown-content">
+                <a href="#">Edit </a>
+                <a href="#"> Edit text</a>
+                <a href="#">Tax</a>
+              </div>           
+            </div>
+
+
+         </div>
+      </div>
+    
+    </>
+  
   )
 }
 
 export default Sidebar
+
+
